7d4069585f48d74b281d464758273c02e17bfb20
Remi Gau <remi_gau@hotmail.com>
2021-08-05 18:30:44 +0200

    mh_style autofix

--- S2/cline.m
+++ S2/cline.m
@@ -27,8 +27,8 @@
 % cline(sqrt(t).*sin(t), sqrt(t).*cos(t)); view(3)                       % Example 1
 % cline(sqrt(t).*sin(t), sqrt(t).*cos(t), t); view(3)                    % Example 2
 % cline(sqrt(t).*sin(t), sqrt(t).*cos(t), t, rand(size(t))); view(3)     % Example 3
-% cline(sqrt(t).*sin(t), sqrt(t).*cos(t), [], rand(size(t))); view(3)	 % Example 4
-% 
+% cline(sqrt(t).*sin(t), sqrt(t).*cos(t), [], rand(size(t))); view(3)    % Example 4
+%
 %
 % Author & Version
 % ================
@@ -41,10 +41,10 @@
 % ====
 % This function uses the edges of a patch to represent the colored 2D/3D-line. The marker-related
 % properties (i.e. 'maker','markersize','markeredgecolor','markerfacecolor') can be used as with a
-% regular line. 
+% regular line.
 %
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-% The line-related properties (i.e. 'linestyle','linewidth') WILL HAVE NO EFFECT 
+% The line-related properties (i.e. 'linestyle','linewidth') WILL HAVE NO EFFECT
 % while displaying the line on screen, but will change the output when printing to file !!!
 % !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! !!!!!
 %
@@ -53,46 +53,46 @@
 % versions of Matlab.
 %
 
-% License 
+% License
 % =======
 % Copyright (c) 2009, Sebastian Hölz
 % All rights reserved.
-% 
-% Redistribution and use in source and binary forms, with or without 
-% modification, are permitted provided that the following conditions are 
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
 % met:
-% 
-%     * Redistributions of source code must retain the above copyright 
+%
+%     * Redistributions of source code must retain the above copyright
 %       notice, this list of conditions and the following disclaimer.
-%     * Redistributions in binary form must reproduce the above copyright 
-%       notice, this list of conditions and the following disclaimer in 
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
 %       the documentation and/or other materials provided with the distribution
-%       
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.
 
 function h = cline(x, y, z, cdata)
 
     % Check input arguments
-    error(nargchk(2, 4, nargin))
-    if ~isnumeric(x) || ~isnumeric(y) || ~isvector(x) || ~isvector(y) || length(x)~=length(y); 
-        error('x and y must be numeric and conforming vectors'); 
+    error(nargchk(2, 4, nargin));
+    if ~isnumeric(x) || ~isnumeric(y) || ~isvector(x) || ~isvector(y) || length(x) ~= length(y)
+        error('x and y must be numeric and conforming vectors');
     end
-    if (nargin == 3 && (~isnumeric(z) || ~isvector(z) || length(x)~=length(z))) || ...
-       (nargin == 4 && ~isempty(z) && (~isnumeric(z) || ~isvector(z) || length(x)~=length(z))) ...
-       (nargin == 4 && (~isnumeric(cdata) || ~isvector(cdata) || length(x)~=length(cdata)))
-        error('z (and cdata) must be a numeric vector and conforming to x and y'); 
+    if (nargin == 3 && (~isnumeric(z) || ~isvector(z) || length(x) ~= length(z))) || ...
+       (nargin == 4 && ~isempty(z) && (~isnumeric(z) || ~isvector(z) || length(x) ~= length(z)))
+        nargin == 4 && (~isnumeric(cdata) || ~isvector(cdata) || length(x) ~= length(cdata));
+        error('z (and cdata) must be a numeric vector and conforming to x and y');
     end
-    
+
     % Draw line as patch
     if nargin == 2
         p = patch([x(:)' nan], [y(:)' nan], 0);
@@ -101,13 +101,16 @@ function h = cline(x, y, z, cdata)
         p = patch([x(:)' nan], [y(:)' nan], [z(:)' nan], 0);
         cdata = [z(:)' nan];
     elseif nargin == 4
-        if isempty(z); z = zeros(size(x)); end
+        if isempty(z)
+            z = zeros(size(x));
+        end
         p = patch([x(:)' nan], [y(:)' nan], [z(:)' nan], 0);
         cdata = [cdata(:)' nan];
     end
-    
-    set(p,'cdata', cdata, 'edgecolor','interp','facecolor','none')
-    
-    
+
+    set(p, 'cdata', cdata, 'edgecolor', 'interp', 'facecolor', 'none');
+
     % Create output
-    if nargout == 1; h = p; end
+    if nargout == 1
+        h = p;
+    end
